13374c0f114c6d90b1594f5530dc7aa83198c894
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-09-21 23:01:15 -0400

    Creacion de rutas hijas para 'pages'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,16 @@
-import { PagesComponent } from './pages/pages.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NgModule } from '@angular/core';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { RegisterComponent } from './auth/register/register.component';
 // import { CommonModule } from '@angular/common'; // Ofrece uso de las direcivas(ngIf, ngFor)
+// Modulos
+import { PagesRoutingModule } from './pages/pages.routing';
 
 import { RouterModule, Routes } from '@angular/router'
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 
 const routes:Routes=[
-  { path:'', component:PagesComponent, 
-  // Rutas hijas(Secundarias)
-    children:[
-      { path:'dashboard', component:DashboardComponent },
-      { path:'progress', component:ProgressComponent },
-      { path:'grafica1', component:Grafica1Component },
-      { path:'',  redirectTo:'/dashboard', pathMatch:'full' }
-    ]
-  },
+
   // Rutas primarias
   { path:'login', component:LoginComponent },
   { path:'register', component:RegisterComponent },
@@ -31,7 +21,8 @@ const routes:Routes=[
 @NgModule({
   imports: [
     // CommonModule
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
   ],
   exports: [ RouterModule ]
 })
