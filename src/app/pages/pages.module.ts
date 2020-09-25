7e34b1838161b27c94d2064122fc20906feeae57
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-09-24 23:43:00 -0400

    Enlazando el input para que cambie el valor de la barra de progreso del componente 'progress

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,8 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'
+import {FormsModule} from '@angular/forms'
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -17,9 +19,10 @@ import { PagesComponent } from './pages.component';
     PagesComponent
   ],
   imports: [
+    FormsModule,
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
   ],
   exports: [
     DashboardComponent,
