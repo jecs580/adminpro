fa58ace19cfdb3b0b0e55c99c87a4e0ddae1bd2e
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-09-28 22:56:31 -0400

    Creando un componente 'dona'con la estructura de de dona y mostrandolo en el componente 'grafica1'

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+ //Modulos
+ import { ChartsModule } from 'ng2-charts';
+
 import { IncrementadorComponent } from './incrementador/incrementador.component';
-import { FormsModule } from '@angular/forms'
+import { DonaComponent } from './dona/dona.component'
 
 
 @NgModule({
-  declarations: [IncrementadorComponent],
+  declarations: [IncrementadorComponent, DonaComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ChartsModule
   ],
   exports:[
-    IncrementadorComponent
+    IncrementadorComponent,
+    DonaComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,22 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { MultiDataSet, Label, Color } from 'ng2-charts';
+
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
-  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData: MultiDataSet = [
-    [350, 450, 100],
+export class Grafica1Component {
+  public labels1 :string[]=['Pan', 'Refresco', 'Tacos'];
+  public data1= [
+    [10, 15, 40],
   ];
-  public  colors:Color[]=[
-    {backgroundColor:['#6857E6','#009FEE','#F02059']}
-  ]
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
