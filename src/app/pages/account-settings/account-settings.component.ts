1c3f4f8eb2ffc83e546add5482716ec8a98291c8
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-10-01 23:14:14 -0400

    Agregando un check en el color seleccionado para cambiar de tema

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,16 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
   public linkTheme = document.querySelector('#theme');
+  public links:NodeListOf<Element>;
   constructor() { }
 
   ngOnInit(): void {
-    
-
+    this.links= document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
   }
   changeTheme(theme:string){
     
     const url=`./assets/css/colors/${theme}.css`
     this.linkTheme.setAttribute('href',url);
     localStorage.setItem('theme',url);
+    this.checkCurrentTheme();
+  }
+  checkCurrentTheme(){
+    this.links.forEach(e =>{
+      e.classList.remove('working');
+      const colorTheme = e.getAttribute('data-theme');
+      const themeUrl = `./assets/css/colors/${colorTheme}.css`;
+      const currentTheme =  this.linkTheme.getAttribute('href');
+      if(themeUrl === currentTheme){
+        e.classList.add('working');
+      }
+
+    })
   }
 }
