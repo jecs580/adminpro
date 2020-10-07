b2deb1f54f85ee15731b82d7666777546e5ff009
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-10-06 22:58:08 -0400

    Uso del metodo 'retry' en los observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators'
 
 @Component({
   selector: 'app-rxjs',
@@ -19,11 +20,16 @@ export class RxjsComponent{
           observer.complete();
         }
         if (i === 2) {
+          i=0;
+          // console.log('error de i es 2')
           observer.error('i llego al valor de 2');
         }
       },1000)
     });
-    obs$.subscribe(
+    obs$.pipe(
+      // El pipe sirve para poder transformar la informacion que fluje antes de usarla, reintentar hacer.
+      retry(1) // Opcionalmente colocamos las veces que reitentara.
+    ).subscribe(
       valor=>console.log('subs:',valor),
       err=>console.warn(err),
       () =>console.log('completo')
