27f1c2207b4633257be1ae07d62e8c82e786b3e2
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-10-07 23:16:38 -0400

    Usando el observable 'interval' y de los operadores take y map

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,41 +1,51 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators'
+import { Observable, interval } from 'rxjs';
+import { retry, take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class RxjsComponent{
-  constructor() { 
-    const obs$ = new Observable(observer=>{
-      let i =-1;
-      const intervalo = setInterval(()=>{
+export class RxjsComponent {
+  constructor() {
+    // this.retornaObservable().pipe(
+    //   // El pipe sirve para poder transformar la informacion que fluje antes de usarla, reintentar hacer.
+    //   retry(1) // Opcionalmente colocamos las veces que reitentara.
+    // ).subscribe(
+    //   valor=>console.log('subs:',valor),
+    //   err=>console.warn(err),
+    //   () =>console.log('completo')
+    // );
+
+    // this.retornaIntervalo().subscribe(console.log);
+    this.retornaIntervalo().subscribe((valor) => console.log(valor));
+  }
+
+  retornaIntervalo(): Observable<number> {
+    // Funcion que instancia un observable y transforma la salida.
+    return interval(1000).pipe(
+      // interval es un observable que se ejecutará cada determinado tiempo de manera infinita
+      take(4), // Especifica el numero de intervalos que hara
+      map((i) => i + 1) // Especifica que por cada valor encontrado q valor retornara
+    );
+  }
+
+  retornaObservable(): Observable<number> {
+    let i = -1;
+    return new Observable<number>((observer) => {
+      const intervalo = setInterval(() => {
         i++;
         observer.next(i);
-        if(i===4){
+        if (i === 4) {
           clearInterval(intervalo);
           observer.complete();
         }
         if (i === 2) {
-          i=0;
           // console.log('error de i es 2')
           observer.error('i llego al valor de 2');
         }
-      },1000)
+      }, 1000);
     });
-    obs$.pipe(
-      // El pipe sirve para poder transformar la informacion que fluje antes de usarla, reintentar hacer.
-      retry(1) // Opcionalmente colocamos las veces que reitentara.
-    ).subscribe(
-      valor=>console.log('subs:',valor),
-      err=>console.warn(err),
-      () =>console.log('completo')
-      
-    );
-   }
-
-
+  }
 }
