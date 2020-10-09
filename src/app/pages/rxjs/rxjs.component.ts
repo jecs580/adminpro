00cb0f9982aa01cd7501711d94e8750d5134f2d2
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-10-08 22:36:02 -0400

    Llaamando al metodo ngOnDestroy para llamar al unsubscribe

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
 import { retry, take, map, filter } from 'rxjs/operators';
 
 @Component({
@@ -7,7 +7,10 @@ import { retry, take, map, filter } from 'rxjs/operators';
   templateUrl: './rxjs.component.html',
   styles: [],
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy{
+
+  public intervalSubs:Subscription;
+
   constructor() {
     // this.retornaObservable().pipe(
     //   // El pipe sirve para poder transformar la informacion que fluje antes de usarla, reintentar hacer.
@@ -19,14 +22,18 @@ export class RxjsComponent {
     // );
 
     // this.retornaIntervalo().subscribe(console.log);
-    this.retornaIntervalo().subscribe((valor) => console.log(valor));
+    this.intervalSubs = this.retornaIntervalo().subscribe((valor) => console.log(valor));
+  }
+  ngOnDestroy(): void {
+    // Metodo que se ejecutara cuando se cambie de componente.
+    this.intervalSubs.unsubscribe();
   }
 
   retornaIntervalo(): Observable<number> {
     // Funcion que instancia un observable y transforma la salida.
     return interval(100).pipe(
       // interval es un observable que se ejecutará cada determinado tiempo de manera infinita
-      take(10), // Especifica el numero de intervalos que hara
+      // take(10), // Especifica el numero de intervalos que hara
       map((i) => i + 1), // Especifica que por cada valor encontrado q valor retornara
       filter(valor => valor%2===0),  // Si devuelve false, el operador take no se dispara, 
       //solo hara la cuenta cuando cumpla la condicion
