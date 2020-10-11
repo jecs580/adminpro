b506acb065587176d233106664dc5fa5a2c09f49
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-10-10 23:03:33 -0400

    Implementado el OnDestroy en breadcrumbs para usar el unsubscribe para evitar duplicar las subscripciones

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,29 +1,34 @@
 import { ActivationEnd, Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styles: [],
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy{
   public title: string;
+  public titleSubs$: Subscription;
   constructor(private router: Router) {
-    this.getDataRuta();
+    this.titleSubs$ = this.getDataRuta().subscribe((data) => {
+      this.title = data.title;
+      console.log(data);
+      
+      document.title = `AdminPro - ${data.title}`;
+    });
   }
-  getDataRuta(){
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof ActivationEnd),
-        // instanceOf verifica si event es una instancia(objeto) de la Clase ActivationEnd
-        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
-        map((event: ActivationEnd) => event.snapshot.data)
-      )
-      .subscribe((data) => {
-        console.log(data);
-        this.title = data.title;
-        document.title= `AdminPro - ${data.title}`;
-      });
+  ngOnDestroy(): void {
+    this.titleSubs$.unsubscribe();
+  }
+  getDataRuta() {
+    // Hacemos la limpieza de la data
+    return this.router.events.pipe(
+      filter((event) => event instanceof ActivationEnd),
+      // instanceOf verifica si event es una instancia(objeto) de la Clase ActivationEnd
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
   }
 }
