4d244b0925a5c7ca5ab7190894f9cf4a6d34cf5a
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-11-07 01:29:43 -0400

    Validaciones del formulario de registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -8,16 +8,35 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+  public formSubmitted:boolean=false;
+
   public registerForm = this.fb.group({
-    name:['Jorge', [Validators.required, Validators.minLength(3)]],
-    email:['test100@gmail.com',Validators.required],
-    password:['123456',Validators.required],
-    password2:['123456',Validators.required],
+    name:['', [Validators.required, Validators.minLength(3)]],
+    email:['',[Validators.required, Validators.email]],
+    password:['',Validators.required],
+    password2:['',Validators.required],
     terminos:[false,Validators.required],
   });
 
   constructor(private fb: FormBuilder) { }
   crearUsuario(){
+    this.formSubmitted=true;
     console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      console.log('Posteando formulario');
+    } else{
+      console.log('Formulario no es correcto...');
+      
+    }
+  }
+  campoNoValido( campo:string):Boolean{
+    if(this.registerForm.get(campo).invalid && this.formSubmitted){
+       return true;
+    } else{
+      return false;
+    }
+  }
+  aceptaTerminos(){
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
   }
 }
