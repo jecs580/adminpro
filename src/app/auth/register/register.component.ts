8fcb9a3cb38ecbacb6c43f5f7f32e8e85b9ef111
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-11-06 23:06:00 -0400

    Creacion de formulario para registro de usuarios usando 'Reactive Forms'

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: [ './register.component.css' ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public registerForm = this.fb.group({
+    name:['Jorge', [Validators.required, Validators.minLength(3)]],
+    email:['test100@gmail.com',Validators.required],
+    password:['123456',Validators.required],
+    password2:['123456',Validators.required],
+    terminos:[false,Validators.required],
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+  crearUsuario(){
+    console.log(this.registerForm.value);
   }
-
 }
