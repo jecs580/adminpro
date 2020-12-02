6543eb2c4780236f59477c8086eeff304625bfb3
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-12-01 23:31:09 -0400

    Mostrandolo en el componente usuarios 2/2

--- src/app/pages/mantenimientos/usuarios/usuarios.component.ts
+++ src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -11,13 +11,32 @@ import { Component, OnInit } from '@angular/core';
 export class UsuariosComponent implements OnInit {
   public totalUsuarios:number= 0;
   public usuarios:Usuario[]=[];
+  public desde:number=0;
+  public limitmax=false;
+  public limitmin=false;
   constructor(private usuarioService:UsuarioService) { }
   ngOnInit(): void {
-    this.usuarioService.cargarUsuarios(0)
+    this.cargarUsuarios();
+  }
+  cargarUsuarios(){
+    this.usuarioService.cargarUsuarios(this.desde)
     .subscribe(({total,usuarios})=>{
       this.totalUsuarios = total;
       this.usuarios=usuarios;
     });
   }
-
+  cambiarPagina(valor:number){
+    this.desde+=valor;
+    this.limitmax=false;
+    this.limitmin=false;
+    if (this.desde < 0) {
+      this.desde=0;
+      this.limitmin=true;
+    }
+    else if (this.desde >= this.totalUsuarios) {
+      this.desde -= valor;
+      this.limitmax=true; 
+    }
+    this.cargarUsuarios();
+  }
 }
