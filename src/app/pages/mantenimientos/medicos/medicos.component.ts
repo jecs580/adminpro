19490648756a52619a70da6bfed5f910a002e276
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-12-20 23:50:58 -0400

    Listado de medicos

--- src/app/pages/mantenimientos/medicos/medicos.component.ts
+++ src/app/pages/mantenimientos/medicos/medicos.component.ts
@@ -1,4 +1,7 @@
+import { ModalImagenService } from 'src/app/services/modal-imagen.service';
+import { MedicoService } from './../../../services/medico.service';
 import { Component, OnInit } from '@angular/core';
+import { Medico } from 'src/app/models/medico.model';
 
 @Component({
   selector: 'app-medicos',
@@ -8,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicosComponent implements OnInit {
 
-  constructor() { }
+  public cargando:boolean=true;
+  public medicos:Medico[]=[];
+  constructor(private medicoService:MedicoService,
+    private modalImagenService:ModalImagenService) { }
 
   ngOnInit(): void {
+    this.cargarMedicos();
   }
-
+  cargarMedicos(){
+    this.cargando=true;
+    this.medicoService.cargarMedicos()
+    .subscribe(resp=>{
+      this.cargando=false;
+      this.medicos=resp;
+      
+    })
+  }
+  abrirModal(medico:Medico){
+    this.modalImagenService.abrirModal('medicos',medico._id,medico.img);
+  }
 }
