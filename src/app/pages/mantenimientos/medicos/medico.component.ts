b91ee8527a321aff25578c0138b8660a0cc03a81
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-12-23 23:17:43 -0400

    Cargando datos sobre el medico y la lista de hospitales en el formulario

--- src/app/pages/mantenimientos/medicos/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,3 +1,6 @@
+import { Hospital } from 'src/app/models/hospital.model';
+import { HospitalService } from './../../../services/hospital.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class MedicoComponent implements OnInit {
-
-  constructor() { }
+  public medicoForm:FormGroup;
+  public hospitales:Hospital[]=[];
+  public hospitalSeleccionado:Hospital;
+  constructor(
+    private fb:FormBuilder,
+    private hospitalService:HospitalService
+    ) { }
 
   ngOnInit(): void {
+    this.cargarHospitales();
+    this.medicoForm = this.fb.group({
+      name:['Jorge',Validators.required],
+      hospital:['',Validators.required]
+    });
+    this.medicoForm.get('hospital').valueChanges
+    .subscribe(resp=>{
+      this.hospitalSeleccionado=this.hospitales.find(h=>h._id===resp);
+      
+    })
   }
-
+  cargarHospitales(){
+    this.hospitalService.cargarHospitales()
+    .subscribe(
+      (resp:Hospital[])=>{
+        this.hospitales=resp;
+      }
+    )
+  }
+  guardarMedico(){
+    console.log(this.medicoForm.value);
+    // this.hospitalService.
+  }
 }
