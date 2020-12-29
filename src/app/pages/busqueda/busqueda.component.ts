330792073bb97573c426c380eea3654c9041d005
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-12-28 22:58:24 -0400

    Mostrando la informacion de la busqueda

--- src/app/pages/busqueda/busqueda.component.ts
+++ src/app/pages/busqueda/busqueda.component.ts
@@ -1,5 +1,9 @@
+import { Hospital } from './../../models/hospital.model';
+import { Usuario } from './../../models/usuario.model';
+import { BusquedasService } from './../../services/busquedas.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Medico } from 'src/app/models/medico.model';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,16 +12,27 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BusquedaComponent implements OnInit {
-
-  constructor(private activatedRoute:ActivatedRoute) { }
+  public usuarios:Usuario[]= [];
+  public medicos:Medico[]= [];
+  public hospitales:Hospital[]= [];
+  constructor(private activatedRoute:ActivatedRoute,
+              private busquedasService:BusquedasService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(
       ({termino})=>{
         console.log(termino);
-        
+        this.busquedaGlobal(termino)    
       }
     )
   }
-
+  busquedaGlobal(termino:string){
+    this.busquedasService.busquedaGlobal(termino)
+    .subscribe((resp:any)=>{
+      this.usuarios = resp.usuarios;
+      this.medicos=resp.medicos;
+      this.hospitales =  resp.hospitales;
+      console.log(this.medicos);
+    })
+  }
 }
